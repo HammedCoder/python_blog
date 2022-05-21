adbc1f9ec58ceadac7e8b21f2308bc970c21141d
HammedCoder <99066309+HammedCoder@users.noreply.github.com>
2022-05-21 01:53:16 +0100

    change date format

--- python_blog/settings.py
+++ python_blog/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = 'static/'
 
 # here """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
